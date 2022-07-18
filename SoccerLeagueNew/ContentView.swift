7b737d9448c85ec18431bb286a64ca228279e1bf
picnicodesm <offiksm0903@naver.com>
2022-07-18 13:09:50 +0900

    Initial Commit

---
+++ SoccerLeagueNew/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SoccerLeagueNew
+//
+//  Created by 김상민 on 2022/07/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SoccerLeagueNew/SoccerLeagueNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoccerLeagueNewApp.swift
+//  SoccerLeagueNew
+//
+//  Created by 김상민 on 2022/07/18.
+//
+
+import SwiftUI
+
+@main
+struct SoccerLeagueNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
